f41acaeeb27bcdd711f6e209ffacf80e5473f140
Thu Yein <thuyein1996alpha@gmail.com>
2025-05-18 18:23:06 +0700

    format navbar & sidebar

---
+++ app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -0,0 +1,16 @@
+"use client"
+import Link from 'next/link'
+import styles from './menuLink.module.css'
+import { usePathname } from 'next/navigation'
+
+const MenuLink = ({item}) => {
+
+    const pathname = usePathname()
+    return (
+        <div href={item.path} className={`${styles.container} ${pathname === item.path && styles.active}`}>
+            {item.icon}
+            {item.title}
+        </div>
+    )
+}
+export default MenuLink
